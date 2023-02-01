af93714e67dc2951ccec8280067fc1b9c6bc707f
giracle <clement.girardin@divtec.ch>
2023-02-01 11:09:42 +0100

    Création scan pour la caméra

--- app/src/main/java/com/example/scanner/MainActivity.java
+++ app/src/main/java/com/example/scanner/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.scanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         btn_scan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                startActivity(new Intent(getApplicationContext(),ScanCodeQrActivity.class));
             }
         });
     }
--- app/src/main/java/com/example/scanner/ScanCodeQrActivity.java
+++ app/src/main/java/com/example/scanner/ScanCodeQrActivity.java
@@ -4,11 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ScanCodeQrActivity extends AppCompatActivity {
+import com.google.zxing.Result;
 
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class ScanCodeQrActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+
+    ZXingScannerView scannerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_code_qr);
+
+        scannerView = new ZXingScannerView(this);
+        setContentView(scannerView);
+    }
+
+    @Override
+    public void handleResult(Result result) {
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        scannerView.stopCamera();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        scannerView.setResultHandler(this);
+        scannerView.startCamera();
     }
 }
