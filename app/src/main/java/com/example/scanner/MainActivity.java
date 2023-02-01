fe1ee39da892791cb8654c881e1a82c279321d08
giracle <clement.girardin@divtec.ch>
2023-02-01 10:53:48 +0100

    Ajout d'un action au clique du bouton et d'une nouvelle activité

--- app/src/main/java/com/example/scanner/MainActivity.java
+++ app/src/main/java/com/example/scanner/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.scanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_scan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_scan = (Button) findViewById(R.id.btn_scan);
+
+        btn_scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
