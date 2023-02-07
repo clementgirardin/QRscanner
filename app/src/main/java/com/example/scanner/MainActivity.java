c3215461f82c106c17dd80c222a94820ab2de014
giracle <clement.girardin@divtec.ch>
2023-02-07 11:00:05 +0100

    ajout librairie open street map

--- app/src/main/java/com/example/scanner/MainActivity.java
+++ app/src/main/java/com/example/scanner/MainActivity.java
@@ -20,19 +20,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Plugin.Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
-
         // Récupération par id
         btn_scan = (Button) findViewById(R.id.btn_scan);
         txt_resultat = (TextView) findViewById(R.id.txt_resultat);
-//        MapView mapView = findViewById(R.id.mapview);
-//
-//        mapView.getMapAsync(new OnMapReadyCallback()){
-//            @Override
-//            public void onMapReady(MapboxMap mapboxMap){
-//                mapboxMap.setStyle();
-//            }
-//        }
+
 
         // Lance l'activité scan_code_qr au clique du bouton
         btn_scan.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/scanner/MapBox.java
+++ app/src/main/java/com/example/scanner/MapBox.java
@@ -4,29 +4,21 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.maps.MapView;
+import com.mapbox.maps.MapView;
 import com.mapbox.maps.MapboxMap;
-import com.mapbox.maps.plugin.Plugin;
 
 
 public class MapBox extends AppCompatActivity {
 
+    private MapView mapView;
     MapboxMap mapboxMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_map_box);
 
-        Plugin.Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
+        MapView mapView = findViewById(R.id.mapView);
 
-        // Récupération par id
-        MapView mapView = findViewById(R.id.ma);
-
-        mapView.getMapAsync(new OnMapReadyCallback()){
-            @Override
-            public void onMapReady(MapboxMap mapboxMap){
-                mapboxMap.setStyle();
-            }
-        }
     }
 }
