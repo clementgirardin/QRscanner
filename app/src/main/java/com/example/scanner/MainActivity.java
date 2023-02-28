8c3310185bc1adaf65d9a1c33aaecbcf6f76a535
giracle <clement.girardin@divtec.ch>
2023-02-28 13:44:16 +0100

    Changement d'emplacement code MainActivity vers mainFragment

--- app/src/main/java/com/example/scanner/MainActivity.java
+++ app/src/main/java/com/example/scanner/MainActivity.java
@@ -1,35 +1,17 @@
 package com.example.scanner;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_scan;
-    Button showMap;
-    public static TextView txt_resultat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Récupération par id
-        btn_scan = (Button) findViewById(R.id.btn_scan);
-        txt_resultat = (TextView) findViewById(R.id.txt_resultat);
-
-        // Lance l'activité scan_code_qr au clique du bouton
-        btn_scan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(),ScanCodeQrActivity.class));
-            }
-        });
 
         // Test clique du bouton pour afficher la map
 //        showMap.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/scanner/main.java
+++ app/src/main/java/com/example/scanner/main.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class main extends Fragment {
 
+    Button btn_scan;
+    Button showMap;
+    public static TextView txt_resultat;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -17,7 +22,17 @@ public class main extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
+        // Récupération par id
+        btn_scan = btn_scan.findViewById(R.id.btn_scan);
+        txt_resultat = txt_resultat.findViewById(R.id.txt_resultat);
 
+        // Lance l'activité scan_code_qr au clique du bouton
+        btn_scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
         return view;
     }
